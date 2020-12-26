48bce63cdaa4e7c5c071fba620e3b900ca7a5bfd
sunjoo0115 <sunjoohwang0115@gmail.com>
2020-12-27 02:20:33 +0900

    fifth commit

---
+++ js/index_scrolltrigger.js
@@ -0,0 +1,14 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".nav-bar", {
+  scrollTrigger: {
+    trigger: ".index__top",
+    start: "top 6rem",
+    markers: true,
+    toggleActions: "play none none reset",
+  },
+
+  scrub: 1,
+  backgroundColor: "rgba(255, 255, 255, 0.15)",
+  backdropFilter: "blur(5px)",
+});
