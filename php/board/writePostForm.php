fbf06c12008f25c645b8ad0ab81bf0dca44947d7
SunjooHwang <sonne.hwang.94@gmail.com>
2021-07-17 18:50:26 +0900

    writePostForm include path updated

--- php/board/writePostForm.php
+++ php/board/writePostForm.php
@@ -1,6 +1,8 @@
 <?php
-    include $_SERVER['DOCUMENT_ROOT'].'/forum/php/session.php';
-    include $_SERVER['DOCUMENT_ROOT'].'/forum/php/checkSignSession.php';
+    // include $_SERVER['DOCUMENT_ROOT'].'/forum/php/session.php';
+    // include $_SERVER['DOCUMENT_ROOT'].'/forum/php/checkSignSession.php';
+    include '../session.php';
+    include '../checkSignSession.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
